ee973615fe7b9c78348652476cc5b9c584c849a1
tkylin <tkylin@vip.qq.com>
2017-06-16 01:56:11 +0800

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="home">
+    <x-header :left-options="{showBack: false}">移动端管理后台</x-header>
+    <x-img class="icon" :src="user.icon"></x-img>
+    <flexbox>
+      <flexbox-item>
+        <div class="flex-demo">{{user.nickName}}</div>
+      </flexbox-item>
+    </flexbox>
+    <flexbox>
+      <flexbox-item>
+        <div class="flex-demo">ID：{{user.id}}&nbsp;&nbsp;&nbsp;&nbsp; 邀请码：{{user.code}}</div>
+      </flexbox-item>
+    </flexbox>
+    <flexbox>
+      <flexbox-item>
+        <div class="flex-demo">加入时间：{{user.joinDate}}</div>
+      </flexbox-item>
+    </flexbox>
+    <grid class="grid">
+      <grid-item class="grid-item" v-for="menu in gridMenu" :link="{path:menu.url}" :label="menu.title">
+      </grid-item>
+    </grid>
+  </div>
+</template>
+
+<script>
+  import icon from '../assets/icon.jpg';
+  import {Group, Cell, XHeader, XInput, XButton, XImg, Flexbox, FlexboxItem,Grid, GridItem} from 'vux'
+  export default {
+    components: {Group, Cell, XHeader, XInput, XButton, XImg, Flexbox, FlexboxItem,Grid, GridItem},
+    data () {
+      return {
+        user: {
+          icon: icon,
+          nickName: '清风',
+          id: 12345,
+          code: 12345,
+          joinDate: '2017-06-16 00:18'
+        },
+        gridMenu:[
+          {title:'修改密码',url:'/password'},
+          {title:'下属代理',url:'/agency'},
+          {title:'下属玩家',url:'/player'},
+          {title:'充值记录',url:'/record'},
+          {title:'授权代理',url:'/authorize'},
+          {title:'退出登录',url:'/logout'},
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  .home {
+    text-align: center;
+  }
+
+  .home .icon {
+    margin: 10px auto;
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    -moz-border-radius: 100%;
+    -webkit-border-radius: 100%;
+    border: 2px solid rgba(255, 255, 255, 0.8);
+    vertical-align: middle;
+  }
+
+  .flex-demo {
+    text-align: center;
+  }
+
+  .grid{
+    margin-top: 10px;
+  }
+  .grid-item{
+    padding: 20px 0 18px;
+  }
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login">
+    <x-header :left-options="{showBack: false}">移动端管理后台</x-header>
+    <group>
+      <x-input title="账号" type="number" required v-model="account"></x-input>
+      <x-input title="密码" type="password" required v-model="password"></x-input>
+    </group>
+    <group>
+      <x-button type="primary" link="/home">立即登录</x-button>
+    </group>
+  </div>
+</template>
+
+<script>
+  import {Group, Cell, XHeader, XInput, XButton} from 'vux'
+
+  export default {
+      name:'login',
+    components: {
+      Group,
+      Cell,
+      XHeader,
+      XInput,
+      XButton
+    },
+    data () {
+      return {
+        account: '',
+        password: ''
+      }
+    }
+  }
+</script>
+
+<style>
+  .login {
+
+  }
+</style>
